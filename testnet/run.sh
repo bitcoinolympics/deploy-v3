21cc359118131a611cf6c919a94605633db2334d
yuranich <yuri.a.samarin@gmail.com>
2023-04-11 19:48:40 +0500

    deployed Uniswap contracts to testnet

---
+++ testnet/run.sh
@@ -0,0 +1,2 @@
+npm run build
+npx dist/index.js -pk $PK -j https://public-node.testnet.rsk.co -w9 0xcCe3E0BAeFb57e96B6a61aCd5119BdF5C0452178 -ncl tRBTC -o 0x82437eaE4D114EB2c64E5C734eE088EDBaF73A4E -c 1
